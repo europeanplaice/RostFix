e1379c8e65c5a6280cc16f403c5f55baaf6f163c
europeanplaice <paul.thomas.goodfellas@gmail.com>
2023-01-15 00:04:42 +0900

    implementation

--- readme.md
+++ readme.md
@@ -1,4 +1,25 @@
 # RostFix: A Simple Stack Language
 RostFix is a stack-based language heavily inspired by a language PostFix in the book "Design Concepts in Programming Languages" (https://mitpress.mit.edu/9780262201759/design-concepts-in-programming-languages/).
 
+Overall, RostFix works like a stack machine. 
+
 It remains a work in progress.
+
+## Grammar
+A code is a sequence that starts with the token `rostfix`, the number of parameters and RostFix commands.
+If we have a program `rostfix 0 9 3 4 add`, `0` means the number of parameters passed, and `9 3 4 add` is the body of commands.
+
+During the execution, RostFix adds `9`, `3` and `4` to a stack and with `add`, it pops last two values (`3` and `4`) from the stack and calculates `3 + 4`.
+
+### Example
+
+`sample.rf`
+```
+rostfix 1 3 add
+```
+
+In console
+```
+cargo run sample.rf 1
+=> 4
+```
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,45 @@
+use std::collections::VecDeque;
+use std::env;
+use std::fs;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let contents =
+        fs::read_to_string(args.get(1).unwrap()).expect("Should have been able to read the file");
+    let res = parse(
+        contents,
+        args[2..]
+            .to_vec()
+            .iter()
+            .map(|a| a.parse::<i32>().unwrap())
+            .collect(),
+    );
+    match res {
+        Ok(a) => println!("{}", a),
+        Err(b) => println!("{}", b),
+    }
 }
 
-pub fn nget(stack: &mut Vec<i32>) -> Result<(), String> {
+#[derive(Clone, Debug)]
+pub enum Token {
+    Val(i32),
+    Seq(Vec<String>),
+}
+
+pub fn nget(stack: &mut Vec<Token>) -> Result<(), String> {
     if stack.len() < 2 {
         return Err("Not enough values to nget".to_string());
     }
-    let v1 = stack.pop().unwrap();
+    let v1_tok = stack.pop().unwrap();
+    let v1 = match v1_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
 
     if let Some(val) = stack.get(stack.len() - v1 as usize) {
-        stack.push(*val);
+        stack.push(val.clone());
     } else {
         return Err(format!("in nget indexing is not valid: {}", v1).to_string());
     };
@@ -16,67 +47,121 @@ pub fn nget(stack: &mut Vec<i32>) -> Result<(), String> {
     Ok(())
 }
 
-pub fn eq(stack: &mut Vec<i32>) -> Result<(), String> {
+pub fn eq(stack: &mut Vec<Token>) -> Result<(), String> {
     if stack.len() < 2 {
         return Err("Not enough values to eq".to_string());
     }
-    let v1 = stack.pop().unwrap();
-    let v2 = stack.pop().unwrap();
+    let v1_tok = stack.pop().unwrap();
+    let v1 = match v1_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
+    let v2_tok = stack.pop().unwrap();
+    let v2 = match v2_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
     if v2 == v1 {
-        stack.push(1);
+        stack.push(Token::Val(1));
     } else {
-        stack.push(0);
+        stack.push(Token::Val(0));
     }
 
     Ok(())
 }
 
-pub fn gt(stack: &mut Vec<i32>) -> Result<(), String> {
+pub fn gt(stack: &mut Vec<Token>) -> Result<(), String> {
     if stack.len() < 2 {
         return Err("Not enough values to gt".to_string());
     }
-    let v1 = stack.pop().unwrap();
-    let v2 = stack.pop().unwrap();
+    let v1_tok = stack.pop().unwrap();
+    let v1 = match v1_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
+    let v2_tok = stack.pop().unwrap();
+    let v2 = match v2_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
     if v2 > v1 {
-        stack.push(1);
+        stack.push(Token::Val(1));
     } else {
-        stack.push(0);
+        stack.push(Token::Val(0));
     }
 
     Ok(())
 }
 
-pub fn lt(stack: &mut Vec<i32>) -> Result<(), String> {
+pub fn lt(stack: &mut Vec<Token>) -> Result<(), String> {
     if stack.len() < 2 {
         return Err("Not enough values to lt".to_string());
     }
-    let v1 = stack.pop().unwrap();
-    let v2 = stack.pop().unwrap();
+    let v1_tok = stack.pop().unwrap();
+    let v1 = match v1_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
+    let v2_tok = stack.pop().unwrap();
+    let v2 = match v2_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
     if v2 < v1 {
-        stack.push(1);
+        stack.push(Token::Val(1));
     } else {
-        stack.push(0);
+        stack.push(Token::Val(0));
     }
 
     Ok(())
 }
 
-pub fn sel(stack: &mut Vec<i32>) -> Result<(), String> {
+pub fn sel(stack: &mut Vec<Token>) -> Result<(), String> {
     if stack.len() < 3 {
         return Err("Not enough values to swap".to_string());
     }
-    let v1 = stack.pop().unwrap();
-    let v2 = stack.pop().unwrap();
-    let v3 = stack.pop().unwrap();
+    let v1_tok = stack.pop().unwrap();
+    let v1 = match v1_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
+    let v2_tok = stack.pop().unwrap();
+    let v2 = match v2_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
+    let v3_tok = stack.pop().unwrap();
+    let v3 = match v3_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
     if v3 == 0 {
-        stack.push(v1);
+        stack.push(Token::Val(v1));
     } else {
-        stack.push(v2);
+        stack.push(Token::Val(v2));
     }
     Ok(())
 }
 
-pub fn swap(stack: &mut Vec<i32>) -> Result<(), String> {
+pub fn swap(stack: &mut Vec<Token>) -> Result<(), String> {
     if stack.len() < 2 {
         return Err("Not enough values to swap".to_string());
     }
@@ -86,7 +171,7 @@ pub fn swap(stack: &mut Vec<i32>) -> Result<(), String> {
     Ok(())
 }
 
-pub fn pop(stack: &mut Vec<i32>) -> Result<(), String> {
+pub fn pop(stack: &mut Vec<Token>) -> Result<(), String> {
     if stack.len() < 1 {
         return Err("Not enough values to pop".to_string());
     }
@@ -94,94 +179,207 @@ pub fn pop(stack: &mut Vec<i32>) -> Result<(), String> {
     Ok(())
 }
 
-pub fn add(stack: &mut Vec<i32>) -> Result<(), String> {
+pub fn add(stack: &mut Vec<Token>) -> Result<(), String> {
     if stack.len() < 2 {
         return Err("Not enough values to add".to_string());
     }
-    let v1 = stack.pop().unwrap();
-    let v2 = stack.pop().unwrap();
-    stack.push(v2 + v1);
+    let v1_tok = stack.pop().unwrap();
+    let v1 = match v1_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
+    let v2_tok = stack.pop().unwrap();
+    let v2 = match v2_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
+    stack.push(Token::Val(v2 + v1));
     Ok(())
 }
 
-pub fn sub(stack: &mut Vec<i32>) -> Result<(), String> {
+pub fn sub(stack: &mut Vec<Token>) -> Result<(), String> {
     if stack.len() < 2 {
         return Err("Not enough values to sub".to_string());
     }
-    let v1 = stack.pop().unwrap();
-    let v2 = stack.pop().unwrap();
-    stack.push(v2 - v1);
+    let v1_tok = stack.pop().unwrap();
+    let v1 = match v1_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
+    let v2_tok = stack.pop().unwrap();
+    let v2 = match v2_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
+    stack.push(Token::Val(v2 - v1));
     Ok(())
 }
 
-pub fn mul(stack: &mut Vec<i32>) -> Result<(), String> {
+pub fn mul(stack: &mut Vec<Token>) -> Result<(), String> {
     if stack.len() < 2 {
         return Err("Not enough values to mul".to_string());
     }
-    let v1 = stack.pop().unwrap();
-    let v2 = stack.pop().unwrap();
-    stack.push(v2 * v1);
+    let v1_tok = stack.pop().unwrap();
+    let v1 = match v1_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
+    let v2_tok = stack.pop().unwrap();
+    let v2 = match v2_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
+    stack.push(Token::Val(v2 * v1));
     Ok(())
 }
 
-pub fn div(stack: &mut Vec<i32>) -> Result<(), String> {
+pub fn div(stack: &mut Vec<Token>) -> Result<(), String> {
     if stack.len() < 2 {
         return Err("Not enough values to div".to_string());
     }
-    let v1 = stack.pop().unwrap();
-    let v2 = stack.pop().unwrap();
-    stack.push(v2 / v1);
+    let v1_tok = stack.pop().unwrap();
+    let v1 = match v1_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
+    let v2_tok = stack.pop().unwrap();
+    let v2 = match v2_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
+    stack.push(Token::Val(v2 / v1));
     Ok(())
 }
 
-pub fn rem(stack: &mut Vec<i32>) -> Result<(), String> {
+pub fn rem(stack: &mut Vec<Token>) -> Result<(), String> {
     if stack.len() < 2 {
         return Err("Not enough values to rem".to_string());
     }
-    let v1 = stack.pop().unwrap();
-    let v2 = stack.pop().unwrap();
-    stack.push(v2 % v1);
+    let v1_tok = stack.pop().unwrap();
+    let v1 = match v1_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
+    let v2_tok = stack.pop().unwrap();
+    let v2 = match v2_tok {
+        Token::Val(v) => v,
+        Token::Seq(_) => {
+            return Err("must not be seq".to_string());
+        }
+    };
+    stack.push(Token::Val(v2 % v1));
     Ok(())
 }
 
-pub fn parse(command: String, mut args: Vec<i32>) -> Result<i32, String> {
-    let mut stack: Vec<i32> = vec![];
-    let mut splitted = command
-        .split(" ")
-        .map(|x| x.to_string())
-        .collect::<Vec<String>>();
-    if splitted[0] != "postfix".to_string() {
-        return Err("invalid syntax".to_string());
+pub fn exec(splitted: &mut VecDeque<String>, stack: &mut Vec<Token>) -> Result<(), String> {
+    match stack.pop().unwrap() {
+        Token::Val(_) => return Err("must be seq".to_string()),
+        Token::Seq(seq) => splitted.append(&mut VecDeque::from(seq)),
     }
-    if splitted[1].parse::<usize>().unwrap() != args.len() {
-        return Err("invalid syntax".to_string());
+    Ok(())
+}
+
+fn create_seq(splitted: &mut VecDeque<String>) -> Result<Vec<String>, String> {
+    let mut a: Vec<String> = Vec::new();
+    loop {
+        let b = splitted.pop_front().unwrap();
+        if b == ")" {
+            break;
+        }
+        a.push(b);
     }
-    splitted.remove(0);
-    splitted.remove(0);
-    for _ in 0..args.len() {
-        stack.push(args.pop().unwrap());
-    }
-    for elem in splitted.iter() {
+    Ok(a)
+}
+
+fn _parse(splitted: &mut VecDeque<String>, stack: &mut Vec<Token>) -> Result<(), String> {
+    while splitted.len() > 0 {
+        let elem = match splitted.pop_front() {
+            None => {
+                break;
+            }
+            Some(el) => el,
+        };
         if let Ok(val) = elem.parse::<i32>() {
-            stack.push(val);
+            stack.push(Token::Val(val));
         } else {
             match elem.as_str() {
-                "add" => add(&mut stack)?,
-                "sub" => sub(&mut stack)?,
-                "mul" => mul(&mut stack)?,
-                "div" => div(&mut stack)?,
-                "rem" => rem(&mut stack)?,
-                "swap" => swap(&mut stack)?,
-                "pop" => pop(&mut stack)?,
-                "eq" => eq(&mut stack)?,
-                "lt" => lt(&mut stack)?,
-                "gt" => gt(&mut stack)?,
-                "nget" => nget(&mut stack)?,
+                "add" => add(stack)?,
+                "sub" => sub(stack)?,
+                "mul" => mul(stack)?,
+                "div" => div(stack)?,
+                "rem" => rem(stack)?,
+                "swap" => swap(stack)?,
+                "pop" => pop(stack)?,
+                "eq" => eq(stack)?,
+                "lt" => lt(stack)?,
+                "gt" => gt(stack)?,
+                "nget" => nget(stack)?,
+                "exec" => exec(splitted, stack)?,
+                "(" => {
+                    let a = create_seq(splitted)?;
+                    stack.push(Token::Seq(a));
+                }
                 _ => panic!("{}", elem),
             }
         }
     }
-    Ok(stack.last().unwrap().clone())
+    Ok(())
+}
+
+pub fn parse(command: String, mut args: Vec<i32>) -> Result<i32, String> {
+    let mut stack: Vec<Token> = vec![];
+    let mut splitted = command
+        .split(" ")
+        .map(|x| x.to_string())
+        .collect::<VecDeque<String>>();
+    if splitted[0] != "rostfix".to_string() {
+        return Err("A code must start with the constant 'rostfix'.".to_string());
+    }
+    if splitted[1].parse::<usize>().unwrap() != args.len() {
+        return Err("The number of parameters is not correct.".to_string());
+    }
+    splitted.pop_front();
+    splitted.pop_front();
+    for _ in 0..args.len() {
+        stack.push(Token::Val(args.pop().unwrap()));
+    }
+    _parse(&mut splitted, &mut stack)?;
+    match stack.last().unwrap().clone() {
+        Token::Val(v) => return Ok(v),
+        Token::Seq(_) => return Err("error".to_string()),
+    }
+}
+
+#[cfg(test)]
+mod tests_with_brackets {
+    use super::parse;
+
+    #[test]
+    fn test_bracket() {
+        let res = parse("rostfix 1 ( 2 mul ) exec".to_string(), vec![7]);
+        assert_eq!(res, Ok(14));
+
+        let res = parse("rostfix 0 ( 0 swap sub ) 7 swap exec".to_string(), vec![]);
+        assert_eq!(res, Ok(-7));
+    }
 }
 
 #[cfg(test)]
@@ -196,44 +394,53 @@ mod tests {
 
     #[test]
     fn test_calc() {
-        let res = parse("postfix 1 4 add 5 mul 6 sub 7 div".to_string(), vec![3]);
+        let res = parse("rostfix 1 4 add 5 mul 6 sub 7 div".to_string(), vec![3]);
         assert_eq!(res, Ok(4));
         let res = parse(
-            "postfix 5 add mul sub swap div".to_string(),
+            "rostfix 5 add mul sub swap div".to_string(),
             vec![7, 6, 5, 4, 3],
         );
         assert_eq!(res, Ok(-20));
-        let res = parse("postfix 3 4000 swap pop add".to_string(), vec![300, 20, 1]);
+        let res = parse("rostfix 3 4000 swap pop add".to_string(), vec![300, 20, 1]);
         assert_eq!(res, Ok(4020));
-        let res = parse("postfix 1 4 lt 10 add".to_string(), vec![3]);
+        let res = parse("rostfix 1 4 lt 10 add".to_string(), vec![3]);
         assert_eq!(res, Ok(11));
     }
 
     #[test]
     fn test_nget() {
-        let res = parse("postfix 1 1 nget mul".to_string(), vec![5]);
+        let res = parse("rostfix 1 1 nget mul".to_string(), vec![5]);
         assert_eq!(res, Ok(25));
-        let res = parse("postfix 2 1 nget".to_string(), vec![4, 5]);
+        let res = parse("rostfix 2 1 nget".to_string(), vec![4, 5]);
         assert_eq!(res, Ok(4));
-        let res = parse("postfix 4 4 nget 5 nget mul mul swap 4 nget mul add add".to_string(), vec![3, 4, 5, 2]);
+        let res = parse(
+            "rostfix 4 4 nget 5 nget mul mul swap 4 nget mul add add".to_string(),
+            vec![3, 4, 5, 2],
+        );
         assert_eq!(res, Ok(25));
     }
 
     #[test]
     fn test_add_without_args() {
-        let res = parse("postfix 0 1 3 add".to_string(), vec![]);
+        let res = parse("rostfix 0 1 3 add".to_string(), vec![]);
         assert_eq!(res, Ok(4));
     }
 
     #[test]
+    fn test_no_calc() {
+        let res = parse("rostfix 0 1 3 7".to_string(), vec![]);
+        assert_eq!(res, Ok(7));
+    }
+
+    #[test]
     fn test_add_with_args() {
-        let res = parse("postfix 0 1 3 add".to_string(), vec![]);
+        let res = parse("rostfix 1 3 add".to_string(), vec![1]);
         assert_eq!(res, Ok(4));
     }
 
     #[test]
     fn test_sub_without_args() {
-        let res = parse("postfix 0 1 4 sub".to_string(), vec![]);
+        let res = parse("rostfix 0 1 4 sub".to_string(), vec![]);
         assert_eq!(res, Ok(-3));
     }
 }
